14f2a558b9916202b051fdbfc83ad4b03b286518
raphael <gamehotdl@gmail.com>
2024-11-29 13:48:07 +0100

    feat: Enhance development setup with superuser creation and static files handling

--- app/entrypoint.dev.sh
+++ app/entrypoint.dev.sh
@@ -15,6 +15,16 @@ fi
 
 python3 manage.py migrate --settings=nuitinfo.settings.development
 python3 manage.py collectstatic --noinput --settings=nuitinfo.settings.development
+
+cat <<EOF | python manage.py shell
+from django.contrib.auth import get_user_model
+
+User = get_user_model()  # get the currently active user model,
+
+User.objects.filter(username='admin').exists() or \
+    User.objects.create_superuser('admin', 'admin@example.com', 'iutinfo')
+EOF
+
 python3 manage.py runserver 0.0.0.0:8000 --settings=nuitinfo.settings.development
 
 exec "$@"
