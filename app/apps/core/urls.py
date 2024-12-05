04f3c3b66e16311c1d632c2f97f154b90aa7346f
Aldanari <curylorafael945@gmail.com>
2024-12-05 21:36:32 +0100

    Feature/Chalutier : Created branch jeu_chalutier - created placeholder html file - added route chalutier in urls - added view chalutier in view - added auto import feature in views

--- app/apps/core/urls.py
+++ app/apps/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home
+from .views import *
 
 urlpatterns = [
     path("", home),
     path("accueil/", home),
+    path("chalutier/", chalutier)
 ]
--- app/apps/core/views.py
+++ app/apps/core/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def chalutier(request):
+    return render(request, 'chalutier.html')
+
+
+black_list = ['render']
+__all__ = [func for func in dir() if not func.startswith('__') and func not in black_list]
