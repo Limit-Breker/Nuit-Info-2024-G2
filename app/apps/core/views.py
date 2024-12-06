65c971b5420a10be75a61f665f31f81b7b22e671
raphael <gamehotdl@gmail.com>
2024-12-06 02:55:55 +0100

    feat: integrate InformationOrgane model and update home view with context data

--- app/apps/core/views.py
+++ app/apps/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import InformationOrgane
 
 
 def home(request):
-    return render(request, 'home.html')
+    infos = InformationOrgane.objects.all()
+    return render(request, 'home.html', context={'infos': infos})
