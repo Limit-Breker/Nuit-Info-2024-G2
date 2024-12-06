d8981134242d118946c7206b70ef81c94b9f166b
titoutan <titouan.br@protonmail.com>
2024-12-06 07:17:15 +0100

    fix

--- app/apps/core/urls.py
+++ app/apps/core/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from .views import home, jeu_coeur
-from .views import home,jeu_reins
-from .views import foie
+from .views import home, jeu_coeur, jeu_reins, foie
 
 urlpatterns = [
     path("", home, name="home"),
     path("accueil/", home, name="home"),
-    path("coeur/",jeu_coeur)
+    path("coeur/",jeu_coeur, name="coeur"),
     path("reins/",jeu_reins, name="jeu_reins"),
-    path("foie/", foie),
+    path("foie/", foie, name="foie"),
 ]
